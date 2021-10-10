e7b77677bb4fce9d44b926f4bdbb232caadad93e
Andrew Sisk <andysisk@knights.ucf.edu>
2021-10-10 17:48:47 -0400

    main commit

---
+++ Assets/Scripts/CemeraScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CemeraScript : MonoBehaviour
+{
+    public GameObject target;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x, this.transform.position.y, this.transform.position.z);   
+    }
+}
